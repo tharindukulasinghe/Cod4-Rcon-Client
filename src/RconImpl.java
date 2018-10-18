d6101e578b0e4fbffadcd7a54881955d616abb38
rdtharindudk <dilshantharinduk@gmail.com>
2018-10-18 16:05:40 +0530

    refactoring

--- src/RconImpl.java
+++ src/RconImpl.java
@@ -3,7 +3,7 @@ public class RconImpl{
     public static void main(String[] args){
         try {
             Rcon server = new Rcon("209.97.168.90",28961,"scrim123",true,10000,10000);
-            String s = server.sendCommand("map mp_killhouse");
+            String s = server.sendCommand("map mp_crossfire");
             System.out.println(s);
         } catch (Exception e) {
             System.out.println("Error : " + e.getMessage());
